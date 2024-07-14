fe970f0cdf3e15d72318978db88be58f70e67031
Ragav-KS <57057039+Ragav-KS@users.noreply.github.com>
2024-07-14 15:51:58 +0530

    chore: disable sourcemap in production build

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,26 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    //add this property
-    sourcemap: true,
-  },
-  server: {
-    port: 4200,
-  },
-  resolve: {
-    alias: {
-      src: path.resolve(__dirname, "src/"),
+export default defineConfig(({ mode }) => {
+  let isProduction = false;
+
+  if (mode === "production") {
+    isProduction = true;
+  }
+
+  return {
+    plugins: [react()],
+    build: {
+      //add this property
+      sourcemap: !isProduction,
     },
-  },
+    server: {
+      port: 4200,
+    },
+    resolve: {
+      alias: {
+        src: path.resolve(__dirname, "src/"),
+      },
+    },
+  };
 });
